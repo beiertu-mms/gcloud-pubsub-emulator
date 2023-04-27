3f7470b718560ed817581d1e78dde0b08dcaf1e3
Tung Beier <beiertu@mediamarktsaturn.com>
2023-04-27 22:31:19 +0200

    feat(script): enable ports configurations

--- run.sh
+++ run.sh
@@ -1,11 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+if [[ -z "${EMULATOR_PORT}" ]]; then
+	echo "EMULATOR_PORT environment variable is not set. Default to 8681."
+	EMULATOR_PORT=8681
+fi
+
+if [[ -z "${EMULATOR_READY_PORT}" ]]; then
+	echo "EMULATOR_READY_PORT environment variable is not set. Default to 8682."
+	EMULATOR_READY_PORT=8682
+fi
 
 # Start the PubSub client in the background. It will poll for an open PubSub
 # emulator port and create its topics and subscriptions when it's up.
 #
 # After it's done, port 8682 will be open to facilitate the wait-for and
 # wait-for-it scripts.
-(/usr/bin/wait-for localhost:8681 -- env PUBSUB_EMULATOR_HOST=localhost:8681 /usr/bin/pubsubc -debug; nc -lkp 8682 >/dev/null) &
+(
+	/usr/bin/wait-for localhost:"$EMULATOR_PORT" -- env PUBSUB_EMULATOR_HOST=localhost:"$EMULATOR_PORT" /usr/bin/pubsubc -debug
+	nc -lkp "$EMULATOR_READY_PORT" >/dev/null
+) &
 
 # Start the PubSub emulator in the foreground.
-gcloud beta emulators pubsub start --host-port=0.0.0.0:8681 "$@"
+gcloud beta emulators pubsub start --host-port=0.0.0.0:"$EMULATOR_PORT" "$@"
